e951fb36ac9a654835d4d0874b0a4443d02d13ea
Jaakko Pallari <jkpl@lepovirta.org>
2015-10-25 16:37:02 +0000

    Image source as location hash
    Changed the location hash to use image path instead of index. This way
    the images can be reordered and the hash will still point to the correct
    image.

--- src/statelist.js
+++ src/statelist.js
@@ -1,58 +1,107 @@
-function StateList(listItems) {
+function StateList(listItems, hashFunction) {
   var self = this;
-  self.list = listItems;
   self.currentIndex = 0;
+  var isHashAvailable = typeof hashFunction === 'function';
 
-  self.setList = function(listItems) {
-    self.list = listItems;
-    self.currentIndex = 0;
+  self.setList = function(items) {
+    self.list = items;
+    setIndex(0, 0);
+    refreshKeyIndex();
   };
+
+  function setIndex(index, fallback) {
+    if (isValidIndex(index)) {
+      self.currentIndex = index;
+    } else {
+      self.currentIndex = isValidIndex(fallback) ? fallback : 0;
+    }
+    updateCurrentKey();
+    return self.currentItem();
+  }
+
+  function isValidIndex(index) {
+    return typeof index === 'number' && !isIndexOutOfBounds(index);
+  }
+
+  function isIndexOutOfBounds(index) {
+    return isNaN(index) || index < 0 || index > self.lastIndex();
+  }
+
+  function refreshKeyIndex() {
+    self.indexByKey = Object.create(null);
+    if (isHashAvailable) {
+      self.list.forEach(function(item, index) {
+        var key = hashFunction(item);
+        self.indexByKey[key] = index;
+      });
+    }
+  }
+
+  function updateCurrentKey() {
+    if (isHashAvailable) {
+      var item = self.currentItem();
+      if (item) {
+        self.currentKey = hashFunction(item);
+      }
+    }
+  }
+
+  function getByKey(key) {
+    if (!isHashAvailable) {
+      throw 'Can\'t get by ID. No hash function set for state list!';
+    }
+    return self.indexByKey[key];
+  }
+
+  function getByItem(item) {
+    return getByKey(hashFunction(item));
+  }
+
+  function removeByIndex(index) {
+    self.list.splice(index, 1);
+    refreshKeyIndex();
+    return setIndex(self.currentIndex, self.lastIndex());
+  }
 
   self.currentItem = function() {
     return self.list[self.currentIndex];
   };
 
   self.next = function() {
-    setIndex(self.currentIndex + 1, 0);
-    return self.currentItem();
+    return setIndex(self.currentIndex + 1, 0);
   };
-
-  function setIndex(index, fallback) {
-    if (isIndexOutOfBounds(index)) {
-      self.currentIndex = fallback;
-    } else {
-      self.currentIndex = index;
-    }
-  }
-
-  function isIndexOutOfBounds(index) {
-    return index < 0 || index > self.lastIndex();
-  }
 
   self.lastIndex = function() {
     return self.list.length - 1;
   };
 
   self.previous = function() {
-    setIndex(self.currentIndex - 1, self.lastIndex());
-    return self.currentItem();
+    return setIndex(self.currentIndex - 1, self.lastIndex());
   };
 
   self.setCurrentItem = function(item) {
-    var index = self.list.indexOf(item);
-    self.setCurrentIndex(index);
+    var index = getByItem(item);
+    return self.setCurrentIndex(index);
+  };
+
+  self.setCurrentId = function(id) {
+    var index = getByKey(id);
+    return self.setCurrentIndex(index);
   };
 
   self.setCurrentIndex = function(index) {
-    setIndex(index, self.currentIndex);
+    return setIndex(index, self.currentIndex);
   };
 
-  self.removeCurrent = function() {
-    var result = self.list.splice(self.currentIndex, 1);
-    if (result.length > 0) {
-      return result[0];
+  self.removeItem = function(item) {
+    var index = getByItem(item);
+    if (typeof index === 'number') {
+      return removeByIndex(index);
     }
   };
+
+  // init
+  self.setList(listItems);
 }
 
 module.exports = StateList;
